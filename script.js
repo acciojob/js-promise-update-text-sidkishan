d9fc2298ed468663eb7af75c94f10df7504d32d2
Siddharth Kishan <91934415+sidkishan@users.noreply.github.com>
2023-05-27 17:04:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function updateHTML(){
+	let prom = new Promise((resolve) => {
+		setTimeout(() =>{
+			resolve("Hello, world!");
+		}, 1000)
+	});
+	prom.then((data) => {
+		const div = document.getElementById("output");
+		div.innerText = data;
+	})
+}
+updateHTML();
